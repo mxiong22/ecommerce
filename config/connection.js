c36a911b2b60c4dd9e1485845f1ae5460171ed58
moony <moonyxiong@gmail.com>
2022-12-01 22:08:22 -0600

    running code on sql and insomnia

--- config/connection.js
+++ config/connection.js
@@ -4,8 +4,9 @@ const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
+  
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {  
+    host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
